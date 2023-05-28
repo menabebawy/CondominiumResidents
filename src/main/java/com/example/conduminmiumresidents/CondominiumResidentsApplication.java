7d4893bda540f2b51f78c55d63773a5f02d824a1
menabebawy <mena.bebawy@ebcont.com>
2023-05-28 13:43:04 +0200

    add Spring boot to existing project

---
+++ src/main/java/com/example/conduminmiumresidents/CondominiumResidentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.conduminmiumresidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominiumResidentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondominiumResidentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/conduminmiumresidents/CondominiumResidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conduminmiumresidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominiumResidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
