5c0176165721d2cf67e2a3fcca132b480565af3f
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-05-28 13:50:42 +0200

    rename artifacts group

--- src/main/java/com/company/conduminmiumresidents/CondominiumResidentsApplication.java
+++ src/main/java/com/company/conduminmiumresidents/CondominiumResidentsApplication.java
@@ -1,4 +1,4 @@
-package com.example.conduminmiumresidents;
+package com.company.conduminmiumresidents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
